2ad20979117547212d9b1edd0e40b56f666b7f10
zackhitch <zackhitch@users.noreply.github.com>
2018-06-18 13:52:48 -0700

    set up python3 - ZH

---
+++ src/hello_python.py
@@ -0,0 +1,50 @@
+
+def count_to_100():
+    for number in range(1, 101):
+        print(number)
+
+
+def stub_function():
+    pass
+
+
+def multiply_two_numbers(x, y):
+    return x * y
+
+
+def main():
+    print('hello world!')
+
+    # count_to_100()
+    value = multiply_two_numbers(6, 7)
+    print(value)
+
+    # a = 1
+    # b = 'this is a string'
+    # c = [1, 2, 3, 4]
+    # d = (5, 6, 7, 8)
+    # e = {'name': 'Zack', 'number': 1}
+
+    # print(a)
+    # print(b)
+    # print(c)
+    # print(d)
+
+    # print(len(c))
+    # c.append(42)
+    # print(c)
+    # c.append('will this work?')
+    # print(c)
+    # print(e)
+
+    # print(e['name'])
+    # e['number'] += 9000
+    # print(e['number'])
+
+    # for n in c:
+    #     for m in d:
+    #         print(n * m)
+
+
+if __name__ == "__main__":
+    main()
